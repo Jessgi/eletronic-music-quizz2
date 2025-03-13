6583d6de1986126b7964426c6a4e96a34f1513e3
Jessica Gizela Dev <devjessgizela@gmail.com>
2025-03-13 15:15:11 -0300

    Adicionei playlist no resultado do quizz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { SafeUrlPipe } from './pipes/safe-url.pipe';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuizzComponent } from './components/quizz/quizz.component';
 import { HomeComponent } from './pages/home/home.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     QuizzComponent,
-    HomeComponent
+    HomeComponent,
+    SafeUrlPipe
   ],
   imports: [
     BrowserModule,
--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,78 +1,87 @@
 import { Component, OnInit } from '@angular/core';
-import quizz_questions from "../../../assets/data/quizz_questions.json"
+import quizz_questions from "../../../assets/data/quizz_questions.json";
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
   styleUrls: ['./quizz.component.css']
 })
-
 export class QuizzComponent implements OnInit {
 
-  title:string = ""
+  title: string = "";
+  questions: any[] = [];
+  questionSelected: any;
 
-  questions:any
-  questionSelected:any
+  answers: string[] = [];
+  answerSelected: { text: string; playlist: string } | null = null;
+  playlistLink: string = "";
 
-  answers:string[] = []
-  answerSelected:string =""
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
 
-  questionIndex:number =0
-  questionMaxIndex:number=0
-
-  finished:boolean = false
+  finished: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
-    if(quizz_questions){
-      this.finished = false
-      this.title = quizz_questions.title
+    if (quizz_questions) {
+      this.finished = false;
+      this.title = quizz_questions.title;
 
-      this.questions = quizz_questions.questions
-      this.questionSelected = this.questions[this.questionIndex]
+      this.questions = quizz_questions.questions;
+      this.questionMaxIndex = this.questions.length;
+      this.questionSelected = this.questions[this.questionIndex];
 
-      this.questionIndex = 0
-      this.questionMaxIndex = this.questions.length
-
-      console.log(this.questionIndex)
-      console.log(this.questionMaxIndex)
-    }
-
-  }
-
-  playerChoose(value:string){
-    this.answers.push(value)
-    this.nextStep()
-
-  }
-
-  async nextStep(){
-    this.questionIndex+=1
-
-    if(this.questionMaxIndex > this.questionIndex){
-        this.questionSelected = this.questions[this.questionIndex]
-    }else{
-      const finalAnswer:string = await this.checkResult(this.answers)
-      this.finished = true
-      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results ]
+      console.log("Quiz iniciado...");
+      console.log("Total de perguntas:", this.questionMaxIndex);
     }
   }
 
-  async checkResult(anwsers:string[]){
-
-    const result = anwsers.reduce((previous, current, i, arr)=>{
-        if(
-          arr.filter(item => item === previous).length >
-          arr.filter(item => item === current).length
-        ){
-          return previous
-        }else{
-          return current
-        }
-    })
-
-    return result
+  playerChoose(value: string) {
+    this.answers.push(value);
+    this.nextStep();
   }
 
+  async nextStep() {
+    this.questionIndex++;
+
+    if (this.questionIndex < this.questionMaxIndex) {
+      this.questionSelected = this.questions[this.questionIndex];
+    } else {
+      const finalAnswer: string = await this.checkResult(this.answers);
+      this.finished = true;
+
+      // 🔹 Pegamos o resultado correto do JSON
+      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results];
+
+      // 🔹 Ajustamos a URL para o formato embed do Spotify
+      this.playlistLink = this.answerSelected?.playlist
+        ? this.answerSelected.playlist.replace("open.spotify.com/playlist/", "open.spotify.com/embed/playlist/")
+        : "";
+
+      console.log("Resultado final:", this.answerSelected);
+      console.log("Playlist URL:", this.playlistLink);
+    }
+  }
+
+  async checkResult(answers: string[]): Promise<string> {
+    const result = answers.reduce((previous, current, i, arr) => {
+      return arr.filter(item => item === previous).length > arr.filter(item => item === current).length
+        ? previous
+        : current;
+    });
+
+    return result;
+  }
+
+  restartQuiz() {
+    this.finished = false;
+    this.answers = [];
+    this.answerSelected = null;
+    this.playlistLink = "";
+    this.questionIndex = 0;
+    this.questionSelected = this.questions[this.questionIndex];
+
+    console.log("Quiz reiniciado!");
+  }
 }
